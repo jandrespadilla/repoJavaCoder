825e9e0945487adbeed0661afb2a93b8e69f2fab
Andres Padilla <jandrespadilla@gmail.com>
2024-09-26 21:01:44 -0300

    Subo primera clase

---
+++ src/main/java/com/coderhouse/Class01.java
@@ -0,0 +1,25 @@
+package com.coderhouse;
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		float valorA = 12;
+		float valorB = 22;
+		float valorC;
+		float resultado;
+		
+		//suma
+		resultado = valorA / valorB;
+		
+		System.out.println("El resultado es "+resultado);
+		
+		//incrementos
+		
+		int contador=1;
+		contador++;
+		valorB++;
+		System.out.println(valorB);
+		
+	}
+
+}
