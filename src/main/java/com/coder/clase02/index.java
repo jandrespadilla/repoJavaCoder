e471949fe059ddc9120edadcade1428c9eca1557
Andres Padilla <jandrespadilla@gmail.com>
2024-10-01 19:44:55 -0300

    Subo Segunda calculadora

--- src/main/java/com/coder/clase02/index.java
+++ src/main/java/com/coder/clase02/index.java
@@ -4,13 +4,20 @@ public class index {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int numeroA= 1;
-		int numeroB= 2;
+		float numeroA= 1;
+		float numeroB= 0;
+		String operacion = "+";
+		float resultado=numeroA/numeroB;
 		if (numeroA>numeroB) {
 			System.out.println(numeroA + " mayor a "+ numeroB);
 		}else {
 			System.out.println(numeroA + " menor a "+ numeroB);
 		}
+		if (numeroB!=0) {
+				System.out.println("Resultado "+ resultado);
+			}else {
+				System.out.println("No se puede dividiir por 0");
+    	}
 	}
 
 }
