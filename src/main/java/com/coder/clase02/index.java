8d09ded916c279b01106b04ed2d8b584cf817edd
Andres Padilla <jandrespadilla@gmail.com>
2024-10-01 19:34:14 -0300

    Calculadora simple

--- src/main/java/com/coder/clase02/index.java
+++ src/main/java/com/coder/clase02/index.java
@@ -4,7 +4,13 @@ public class index {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int numeroA= 1;
+		int numeroB= 2;
+		if (numeroA>numeroB) {
+			System.out.println(numeroA + " mayor a "+ numeroB);
+		}else {
+			System.out.println(numeroA + " menor a "+ numeroB);
+		}
 	}
 
 }
